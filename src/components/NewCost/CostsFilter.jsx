204e4c88dbcc877e5471ec41bef7cd402b37c543
ozhadykov <3282088omar@gmail.com>
2023-04-17 09:45:23 +0200

    minor changes

--- src/components/NewCost/CostsFilter.jsx
+++ src/components/NewCost/CostsFilter.jsx
@@ -8,7 +8,7 @@ const CostsFilter = (props) => {
   return (
     <div className="costs-filter">
       <div className="costs-filter__control">
-        <label>Выбор По Году</label>
+        <label>Chose by Year</label>
         <select value={props.year} onChange={yearChangeHandler}>
           <option value="2023">2023</option>
           <option value="2022">2022</option>
